323ff2f281d40e56c624ff85767dc4b70b622865
INDRONIIL <indroniilsil730@gmail.com>
2026-07-10 20:14:54 +0530

    attach

--- emp_prm.py
+++ emp_prm.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 
 df = pd.read_csv(r'employee_promotion_prediction.csv')
 print(df.head())
@@ -15,3 +15,22 @@ print(df.head())
 
 X = df.drop('promoted',axis=1)
 y = df['promoted']
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
+
+scaler=StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = SVC(kernel='rbf')
+model.fit(X_train,X_test)
+
+prediction = model.predict(X_test)
+
+print("Predicted:",prediction)
+print("Actual:",y_test)
+
+print("Accuracy:",accuracy_score(y_test,prediction))
+print("Confusion_matrix:",confusion_matrix(y_test,prediction))
+print("Classification_report:",classification_report(y_test,prediction))
+
