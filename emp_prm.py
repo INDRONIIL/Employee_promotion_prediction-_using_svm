f5d3a9bfca29a9d28490edd662d811279214f2db
INDRONIIL <indroniilsil730@gmail.com>
2026-07-10 20:59:33 +0530

    attach

--- emp_prm.py
+++ emp_prm.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+import matplotlib.pyplot as plt
 
 df = pd.read_csv(r'employee_promotion_prediction.csv')
 print(df.head())
@@ -19,11 +20,12 @@ y = df['promoted']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
 
 scaler=StandardScaler()
+
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 model = SVC(kernel='rbf')
-model.fit(X_train,X_test)
+model.fit(X_train,y_train)
 
 prediction = model.predict(X_test)
 
@@ -34,3 +36,43 @@ print("Accuracy:",accuracy_score(y_test,prediction))
 print("Confusion_matrix:",confusion_matrix(y_test,prediction))
 print("Classification_report:",classification_report(y_test,prediction))
 
+df['promoted'].value_counts().plot(kind='bar')
+plt.title("Promotion Distribution")
+plt.xlabel("Promoted")
+plt.ylabel("Count")
+plt.show()
+
+plt.scatter(df['age'],df['promoted'])
+plt.title("Age vs Promotion")
+plt.xlabel("Age")
+plt.ylabel("Promoted")
+plt.show()
+
+plt.hist(df['salary'],bins=20)
+plt.title("Salary Distribution")
+plt.xlabel("Salary")
+plt.ylabel("Employees")
+plt.show()
+
+plt.scatter(df['salary'],df['promoted'])
+plt.title("salary vs promotion")
+plt.xlabel("Salary")
+plt.ylabel("Promotion")
+plt.show()
+
+plt.scatter(df['performance_score'],df['promoted'])
+plt.title("Performnce score vs promotion")
+plt.xlabel("Performance score")
+plt.ylabel("Promotion")
+plt.show()
+
+plt.scatter(df['years_in_current_role'],df['promoted'])
+plt.title("years_in_current_role vs promotion")
+plt.xlabel("years_in_current_role")
+plt.ylabel("promotion")
+plt.show()
+
+df.groupby('promoted')['salary'].mean().plot(kind='bar')
+plt.title("Average Salary by Promotion")
+plt.ylabel("Average Salary")
+plt.show()
